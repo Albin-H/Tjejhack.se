17f60cd9b7c6fbb8dd590ec453cc5023c31ac0f5
Albin H <57447840+Albin-H@users.noreply.github.com>
2020-05-12 21:37:09 +0200

    12/5
    Försökt koda färdigt spelet och fixat till hemsidan

--- Spel/skript.js
+++ Spel/skript.js
@@ -1,13 +1,28 @@
 let nummer = 1;
 
+function svar(){
+
+let container = document.querySelector("#quiz").fråga.value;
+tal ++;
+
+	if (container == Rätt) {
+		antal++;
+	}
+}
+
 function ladda() {
-	document.querySelector("#svar").innerHTML =`<button class="btn btn-primary">left(50)</button> <button class="btn btn-primary">right(50)</button> <button class="btn btn-primary">down(50)</button>`;
 	document.querySelector("#bakgrund").innerHTML =`<div style="border:1px solid #d3d3d3; background-image: url(Steg${nummer}.png); background-size: cover; margin: auto; width: 100%; height: 100%;">  </div>`;
 	nummer++;
 
 	if( nummer == 14)
 	{
-		document.querySelector("#svar").innerHTML =`<h1>Du Klarade Det !!!</h1>`;
+		document.querySelector("#bakgrund").innerHTML =`<div class="container"><div class="vinst"><h1>Du Klarade Det !!!</h1></div></div>`;
+		document.querySelector("#svar").innerHTML =`<h1> </h1>`;
 
 	}
+}
+
+function myFunction() {
+  var x = document.getElementsByClassName("knappar")[0].id;
+  document.getElementById("test").innerHTML = x;
 }
